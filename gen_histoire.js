5fc5a4d9f93517ca0c21597979818bc35f97ab8e
cyriakleclercq <cyriakleclercq@gmail.com>
2018-11-09 10:36:54 +0100

    les tableaux

---
+++ gen_histoire.js
@@ -0,0 +1,21 @@
+var noms = ['michel varuk','jim carrey','albert einstein','vivaldi','emma watson','renaud', 'eminem','paul rudd','jacques','bernadette','johnny depp','the rock','lea','nina','julien lepers','hugh jackman','jacquie','michel','gotaga','phillipe etchebest','hp lovecraft','bill gates'];
+
+var objets = ['ordinateur','telephone','montre','de a coudre','decapsuleur','ballon','clavier','marteau','piece jaune','lunettes de soleil','sabre','cuillere','fourchette','tasse','manette','avion','miche de pain','velo'];
+
+var temperatures = ['10°c','30°c','5°c','50°c','12°c','100°c','69°c','18°c','37°c','25°c','-30°c','-40°c','28°c','24°c','70°c'];
+
+var lieux = ['floyon','wignehies','barcelone','paris','tokyo','reykjavik','bondy','rio de janeiro','le mans','le touquet','big ben','mont fuji','palais de elysee','mairie de paris','mc donalds','kfc','burger king','montcuq','chutes du niagara','hidepark'];
+
+var verbes = ['coder','copier','coordonner','nager','monter','avoir','aimer','etre passionne','developper','jouer','soulever','evoluer','voter','arnaquer','manger','envoyer','enfourner','accomplir','communiquer'];
+
+var nb_aleatoire_noms = Math.floor((Math.random()*22)+1);
+
+var nb_aleatoire_objets = Math.floor((Math.random()*18)+1);
+
+var nb_aleatoire_temperatures = Math.floor((Math.random()*15)+1);
+
+var nb_aleatoire_lieux = Math.floor((Math.random()*20)+1);
+
+var nb_aleatoire_verbes = Math.floor((Math.random()*19)+1);
+
+var message = prompt("");
